694f5a3e7a5ee263eee0065e5c3faf810945abeb
Чернышев Кирирлл Андреевич <kchernyshev@rtk-element.ru>
2024-12-05 15:19:01 +0700

    Initial

---
+++ src/main/java/ru/coffe/libs/propertygenerator/PropertyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package ru.coffe.libs.propertygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/coffe/libs/propertygenerator/PropertyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coffe.libs.propertygenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
